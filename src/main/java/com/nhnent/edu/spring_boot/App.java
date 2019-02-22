3c23aa00f735fb941a7e0ca369907ed932a85304
신동민/NE <dongmyo@nhnent.com>
2019-02-22 13:57:59 -0800

    * TODO 메세지 수정

--- src/main/java/com/nhnent/edu/spring_boot/App.java
+++ src/main/java/com/nhnent/edu/spring_boot/App.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 // TODO : #6 com.nhnent.edu.spring_boot 패키지에 App.java 생성 및 작성.
+// TODO : #6 create and write a file named `App.java which resides in `com.nhnent.edu.spring_boot` package. 
 @RestController
 @EnableAutoConfiguration
 public class App 
